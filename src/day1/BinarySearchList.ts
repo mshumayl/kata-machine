61750935860b8fda6bea158f36438071ea353391
Shumayl Asmawi <shumayl.asmawi@hp.com>
2023-05-27 23:27:35 +0800

    day1/BinarySearchList.ts

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,24 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let result: boolean = false;
+
+    const half = (arr: number[]): boolean => {
+        let halfpoint = Math.ceil((arr.length-1) / 2);
+        
+        if (arr.length === 0) {
+            return false
+        }
+
+        if (needle === arr[halfpoint]) {
+            return true
+        } else if (needle > arr[halfpoint]) {
+            return half(arr.slice(halfpoint+1, arr.length))
+        } else {
+            return half(arr.slice(0, halfpoint))
+        }
+    }
+
+    result = half(haystack)
+
+    return result
 }
