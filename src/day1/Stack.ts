1b354a5fb42e5c04bb4fdae83116ba3485c74e49
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-05-29 20:50:02 +0800

    day1/Stack.ts

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T,
+    prev: Node<T> | undefined,
+}
+
 export default class Stack<T> {
     public length: number;
-
+    private head: Node<T> | undefined;
     
-
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const newNode = {value: item} as Node<T>;
 
-}
+        this.length++
+
+        if (!this.head) {
+            this.head = newNode;
+            return; 
+        } 
+        
+        newNode.prev = this.head; //Set the prev of newNode to the current head node
+        this.head = newNode; //Set newNode as the new head node
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined
+        }
 
-}
+        this.length--
+
+        const head = this.head;
+        this.head = this.head.prev;
+
+        head.prev = undefined;
+
+        return head.value
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
