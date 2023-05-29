b797ce5e57c265d6bef7d0091e2ffb11be8cd452
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-05-29 20:19:57 +0800

    Reimplement day1/Queue.ts

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,36 +1,37 @@
 type Node<T> = {
     value: T,
-    next: Node<T> | undefined,
+    next: Node<T> | undefined
 }
 
 export default class Queue<T> {
     public length: number;
-    private head: Node<T> | undefined;    
     private tail: Node<T> | undefined;
+    private head: Node<T> | undefined;
 
     constructor() {
-        this.head = this.tail = undefined
         this.length = 0;
+        this.tail = this.head = undefined;
     }
 
     enqueue(item: T): void {
-        this.length++
+        const newNode = {value: item} as Node<T>;
 
-        const newNode = {value: item} as Node<T>
+        this.length++;
 
-        if (!this.tail) { //When queue is empty
-            this.tail = this.head = newNode
+        //If the queue is empty
+        if (!this.tail) {
+            this.tail = this.head = newNode;
             return;
         } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
+            this.tail.next = newNode; //Set the current tail's next to point to newNode
+            this.tail = newNode; //Set newNode as the new tail value
         }
-
     }   
 
     deque(): T | undefined {
+        //If the queue is empty
         if (!this.head) {
-            this.tail = undefined // If the queue is empty, set tail to be undefined too.
+            this.tail = undefined;
             return undefined
         }
 
@@ -38,9 +39,6 @@ export default class Queue<T> {
 
         const head = this.head;
         this.head = this.head.next;
-
-        //Garbage collection -> unlink unused node
-        head.next = undefined
 
         return head.value
     }
