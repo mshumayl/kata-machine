42fb57e0ae155dea9b0c3875ad25ba2ab91bfe50
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-05-30 22:23:23 +0800

    day1/QuickSort.ts

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,10 @@
+
+class BinaryNode {
+    constructor(item) {
+        this.item = BinaryNode
+    }
+}
+
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
 }
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,39 @@
-export default function quick_sort(arr: number[]): void {
+//The recursion function
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo>=hi) {
+        return;
+    }
 
+    const pivotIdx = partition(arr, lo, hi);
+
+    //Inclusive ending
+    qs(arr, lo, pivotIdx - 1); //Low
+    qs(arr, pivotIdx + 1, hi); //High
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let idx = lo - 1; 
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+
+            const tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+    }
+
+    idx++;
+    arr[hi] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
+}
+
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
