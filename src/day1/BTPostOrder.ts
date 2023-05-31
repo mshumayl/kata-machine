91a42fa4e2877e27c217c6a7701ba8c38b89a0f2
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-05-31 23:55:14 +0800

    day1/BTPostOrder.ts

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,18 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    //Base case - if the previous curr has no children
+    if (!curr) {
+        return path;
+    }
 
+    //Perform recursion
+    //Post-order traversal: Walk all the left nodes, then the right nodes, and visit only when there are no left and right nodes.    
+    walk(curr.left, path); 
+    walk(curr.right, path);
+    path.push(curr.value); //Visit the node
+
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
