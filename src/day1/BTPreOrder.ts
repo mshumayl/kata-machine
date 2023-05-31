5c027bed0fd957fd20088ce5509108f40a11cb2e
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-05-31 23:47:03 +0800

    day1/BTPreOrder.ts

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,21 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    //Base case - if the previous curr has no children
+    if (!curr) {
+        return path;
+    }
 
+    //Perform recursion
+    //Pre-recurse
+    path.push(curr.value); //Add value to travelled path
+    
+    //Recurse
+    walk(curr.left, path); //No need to return path, as it is already mutated by reference.
+    walk(curr.right, path);
+
+    //Post-recurse
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
