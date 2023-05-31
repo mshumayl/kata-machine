eb3658eb9b5265d9abf442d0f624a13137dd8931
Shumayl <83513359+mshumayl@users.noreply.github.com>
2023-05-31 23:52:58 +0800

    day1/BTInOrder.ts

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,18 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    //Base case - if the previous curr has no children
+    if (!curr) {
+        return path;
+    }
 
+    //Perform recursion
+    //In-order traversal: Walk all the left nodes and visit only when there are no left nodes.    
+    walk(curr.left, path); 
+    path.push(curr.value); //Visit the node
+    walk(curr.right, path);
+
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
