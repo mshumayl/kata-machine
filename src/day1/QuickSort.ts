f61492bcc3e85b92f1eb6479cfa5ffe218c148e2
mshumayl <mshumayl@gmail.com>
2023-05-31 14:41:26 +0800

    Reimplement day1/QuickSort.ts

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,41 +1,41 @@
-function qs(arr: number[], lo: number, hi: number): void {
+//Gist: https://gist.github.com/mshumayl/cda24c0c5ce6dd903f66c75927900836
+
+function partition(arr: number[], lo: number, hi: number): number { //Return pivot index
+    const pivot = arr[hi]; //There are other ways to implement
+
+    let destIdx = lo - 1; //Start with one spot before the start of the array/partition
+
+    //Walk from `lo` to `hi`
+    for (let i = lo; i < hi; i++) { 
+        if (arr[i] <= pivot) { //If the current element is equal to or smaller than pivot
+            destIdx++; 
+
+            //Swap the current element with the destination element
+            const tmp = arr[i];
+            arr[i] = arr[destIdx];
+            arr[destIdx] = tmp;
+        }
+    }
+
+    destIdx++; //Move destination pointer to the next index
+    arr[hi] = arr[destIdx]; //Swap the `hi` value with the destination value
+    arr[destIdx] = pivot; //Set the destination value as the pivot value (new pivot index)
+
+    return destIdx; //Return the new pivot index
+}
+
+function qs(arr: number[], lo: number, hi: number) {
     //Base case
     if (lo>=hi) {
         return;
     }
 
-    const pivotIdx = partition(arr, lo, hi);
+    const pivotIdx = partition(arr, lo, hi)
 
     qs(arr, lo, pivotIdx-1) //Left side of pivot
     qs(arr, pivotIdx+1, hi) //Right side of pivot
 }
 
-function partition(arr: number[], lo: number, hi: number): number { //Return pivotIdx
-    const pivot = arr[hi];
-    
-    //Destination value to be swapped with walked arrays that match the condition below
-    let destIdx = lo - 1; //Start with -1, and increment every time the condition below matches and after walk is finished
-    
-    //Perform walk
-    for (let walkIdx = lo; walkIdx < hi; walkIdx++) {
-        if (arr[walkIdx] <= pivot) { //If the value of current walked array is smaller than pivot,
-            destIdx++
-            
-            //Swap between current value and the destination value
-            const tmp = arr[walkIdx];
-            arr[walkIdx] = arr[destIdx]
-            arr[destIdx] = tmp;
-        }
-    }
-
-    //After walk
-    destIdx++;
-    arr[hi] = arr[destIdx]; //These are side-by-side swaps
-    arr[destIdx] = pivot; //Move pivot value into its actual position in the sort
-
-    return destIdx; //Return the new pivot index
-}
-
 export default function quick_sort(arr: number[]): void {
-    qs(arr, 0, arr.length - 1)
+    qs(arr, 0, arr.length-1)
 }
