475034b8bde29d6faedd95a17a8ca2d30a9e0c92
mshumayl <mshumayl@gmail.com>
2023-06-03 23:55:41 +0800

    Fix day1/BinarySearchList.ts

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -2,23 +2,24 @@ export default function bs_list(haystack: number[], needle: number): boolean {
 
     let result: boolean = false;
 
-    const half = (lo: number, hi: number): boolean => {
-        let halfpoint = Math.floor((hi-lo+1) / 2);
+    const half = (haystack: number[], needle: number, lo: number, hi: number): boolean => {
 
-        if (hi-lo === 0) {
+        if (lo>hi) {
             return false
         }
+
+        let halfpoint = Math.floor((hi+lo) / 2);
 
         if (needle === haystack[halfpoint]) {
             return true
         } else if (needle > haystack[halfpoint]) {
-            return half(halfpoint+1, hi); //Slicing is very expensive, this can be replaced with just sending a start and end indices into the recursive function
+            return half(haystack, needle, halfpoint+1, hi); //Slicing is very expensive, this can be replaced with just sending a start and end indices into the recursive function
         } else {
-            return half(lo, halfpoint-1);
+            return half(haystack, needle, lo, halfpoint-1);
         }
     }
 
-    result = half(0, haystack.length)
+    result = half(haystack, needle, 0, haystack.length)
 
     return result
 }
