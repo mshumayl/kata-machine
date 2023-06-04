d56bf8ca86fd020816e094ae2c59b70d4028238a
mshumayl <mshumayl@gmail.com>
2023-06-04 22:55:50 +0800

    day1/DFSOnBTS.ts

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,21 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+    //Base case: If we traversed to the leaf of the tree
+    if (!curr) {
+        return false;
+    }
 
+    //Base case: If we find the value
+    if (curr.value === needle) {
+        return true;
+    }
+
+    if (curr.value < needle) {
+        return search(curr.right, needle);
+    }
+
+    return search(curr.left, needle); //Go left if needle is smaller than curr.value
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
