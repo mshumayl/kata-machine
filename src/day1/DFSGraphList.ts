154a11d4e2e5534a300d424bae29fd73b02a20bb
mshumayl <mshumayl@gmail.com>
2023-06-05 23:58:48 +0800

    day1/DSFGraphList.ts

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,55 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+//Similar to MazeSolver
+function walk(
+    graph: WeightedAdjacencyList, 
+    curr: number, 
+    needle: number, 
+    seen: boolean[], 
+    path: number[]): boolean {
 
+
+    if (seen[curr]) { //If the current vertex exists in seen array
+        return false;
+    }
+    
+    //Recursion step
+    //Pre
+    seen[curr] = true;
+    path.push(curr); //Visit the node
+
+    //Base case -- found the needle
+    if (curr === needle) {
+        return true;
+    }
+    
+    //Recurse
+    const list = graph[curr]; //Who is this node connected to
+    for (let i = 0; i < list.length; i++) {
+        const edge = list[i];
+        
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+    
+    //Post
+    path.pop(); //Remove curr after recursion
+
+    return false;
+}
+
+export default function dfs(
+    graph: WeightedAdjacencyList, 
+    source: number, 
+    needle: number): number[] | null {
+
+    const seen: boolean[] = new Array(graph.length).fill(false);
+    const path: number[] = [];
+
+    walk(graph, source, needle, seen, path)
+
+    if (path.length === 0) {
+        return null;
+    }
+
+    return path;
 }
